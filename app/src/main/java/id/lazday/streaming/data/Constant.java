5320d80386972b5e91948700034edf4898c4686c
ainurilmy <wwie1188@gmail.com>
2019-01-02 13:46:04 +0700

    AplikasiAndroid

---
+++ app/src/main/java/id/lazday/streaming/data/Constant.java
@@ -0,0 +1,25 @@
+package id.lazday.streaming.data;
+
+public class Constant {
+
+    /**
+     *  localhost   : http://ip_address/folder_location
+     */
+
+    public static final String BASE_URL     = "http://192.168.43.43/vidstreaming/api/";
+
+
+    public static final String VIDEO_PATH   = BASE_URL.replace( "api/", "assets/video/");
+    public static final String COVER_PATH   = BASE_URL.replace( "api/", "assets/cover/");
+}
+
+
+
+
+
+
+
+
+
+
+//    public static final String BASE_URL     = "http://demo.lazday.com/vidstreaming/api/";
